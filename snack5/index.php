0f694633375e8fff4265e2d8c6528015869074a3
Elsti97 <elsti.shehati@hotmail.it>
2022-12-13 16:14:38 +0100

    Snack 5 done

--- snack5/index.php
+++ snack5/index.php
@@ -1,3 +1,14 @@
 <!-- Snack 5
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
-Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo. -->
+
+<?php
+$paragrafo= 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel, veritatis minus voluptas maiores harum voluptate provident voluptates, quam, magnam nesciunt nobis doloribus aspernatur facere quidem illo saepe at sunt commodi? Lorem ipsum dolor sit amet consectetur adipisicing elit. Et temporibus blanditiis, repudiandae dolor odit nemo. Recusandae modi quibusdam laboriosam sint reiciendis odio rerum qui exercitationem necessitatibus hic repellat ipsum facere. Lorem ipsum dolor sit amet consectetur adipisicing. elit. Et temporibus blanditiis, repudiandae dolor odit nemo, recusandae modi quibusdam laboriosam sint reiciendis odio rerum qui exercitationem necessitatibus hic repellat. ipsum facere.';
+echo "<h1>Paragrafo Originale:</h1>$paragrafo";
+$miniPar = explode( '.', $paragrafo, -1);
+
+foreach($miniPar as $parCol){
+  echo "<ul><li>{$parCol}</li></ul>";
+}
+?>
